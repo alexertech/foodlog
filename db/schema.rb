f11e3251515686cbb32d57f888bb8ec33a6ed47d
Alex B <alexertech@offensive-security.com>
2021-04-13 21:09:36 -0400

    Adding calories

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_154650) do
+ActiveRecord::Schema.define(version: 2021_04_14_003711) do
 
   create_table "entries", force: :cascade do |t|
     t.string "meal_type"
     t.string "descript"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "calories"
   end
 
 end
